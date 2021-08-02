e9247e293069d4e643e82d3388460036ce49e426
Soon Yung Jun <syjun@wc.fnal.gov>
2021-08-02 13:27:22 -0500

    Updated for the latest celeritas

--- app/g4hpc/src/TrackingAction.cc
+++ app/g4hpc/src/TrackingAction.cc
@@ -13,78 +13,84 @@
 #include "physics/base/Primary.hh"
 
 TrackingAction::TrackingAction()
-  : G4UserTrackingAction(),
-    fChunkPhotons(0),
-    fChunkElectrons(0),
-    fMessenger(nullptr)
+    : G4UserTrackingAction()
+    , fChunkPhotons(0)
+    , fChunkElectrons(0)
+    , fMessenger(nullptr)
 {
-  DefineCommands();
+    DefineCommands();
 }
 
 TrackingAction::~TrackingAction()
-{ 
-  delete fMessenger;
+{
+    delete fMessenger;
 }
 
-void TrackingAction::PreUserTrackingAction(const G4Track* track) 
+void TrackingAction::PreUserTrackingAction(const G4Track* track)
 {
-  unsigned int  pid = track->GetDefinition()->GetPDGEncoding();
-  G4ThreeVector pos = track->GetPosition();
-  G4ThreeVector dir = track->GetMomentumDirection();
+    unsigned int  pid = track->GetDefinition()->GetPDGEncoding();
+    G4ThreeVector pos = track->GetPosition();
+    G4ThreeVector dir = track->GetMomentumDirection();
+    unsigned int  tid = track->GetTrackID();
 
-  if(pid == 22) {
-    fPhotonStack.push_back({ParticleDefId{pid},
-	  units::MevEnergy{track->GetKineticEnergy()},
-	    {pos.x(), pos.y(), pos.z()},
-	    {dir.x(), dir.y(), dir.z()},
-	    EventId{0}});
+    if (pid == 22)
+    {
+        fPhotonStack.push_back({ParticleId{pid},
+                                units::MevEnergy{track->GetKineticEnergy()},
+                                {pos.x(), pos.y(), pos.z()},
+                                {dir.x(), dir.y(), dir.z()},
+                                EventId{0},
+                                TrackId{tid}});
 
-    (const_cast<G4Track*>(track))->SetTrackStatus(fStopAndKill);
+        (const_cast<G4Track*>(track))->SetTrackStatus(fStopAndKill);
+    }
+    if (pid == 11)
+    {
+        fElectronStack.push_back({ParticleId{pid},
+                                  units::MevEnergy{track->GetKineticEnergy()},
+                                  {pos.x(), pos.y(), pos.z()},
+                                  {dir.x(), dir.y(), dir.z()},
+                                  EventId{0},
+                                  TrackId{tid}});
 
-  }
-  if(pid == 11) {
-    fElectronStack.push_back({ParticleDefId{pid},
-	  units::MevEnergy{track->GetKineticEnergy()},
-	    {pos.x(), pos.y(), pos.z()},
-	    {dir.x(), dir.y(), dir.z()},
-	    EventId{0}});
+        (const_cast<G4Track*>(track))->SetTrackStatus(fStopAndKill);
+    }
 
-    (const_cast<G4Track*>(track))->SetTrackStatus(fStopAndKill);
-  }
+    // if size of the stack is fChunkElectrons
+    if (fElectronStack.size() == fChunkElectrons)
+    {
+        // TODO: lauch a kernel with this chuck of electrons
 
-  //if size of the stack is fChunkElectrons
-  if( fElectronStack.size() == fChunkElectrons ) {
-    //TODO: lauch a kernel with this chuck of electrons 
+        // reset
+        fElectronStack.clear();
+    }
 
-    //reset 
-    fElectronStack.clear();
-  }
+    // if size of the stack is fChunkPhotons
+    if (fPhotonStack.size() == fChunkPhotons)
+    {
+        // TODO: lauch a kernel with this chuck of photons
 
-  //if size of the stack is fChunkPhotons
-  if(fPhotonStack.size() == fChunkPhotons) {
-    //TODO: lauch a kernel with this chuck of photons
-
-    //reset 
-    fPhotonStack.clear();
-  }
+        // reset
+        fPhotonStack.clear();
+    }
 }
 
 void TrackingAction::DefineCommands()
 {
-  // Define the command directory
-  fMessenger = new G4GenericMessenger(this, "/g4hpc/trackingAction/", 
-                                      "User Tracking Action control");
-            
-  // commands for the size of paticle chunk for tasking
-  auto& chunkElectronCmd
-    = fMessenger->DeclareMethod("setChunkElectrons",
-                                &TrackingAction::SetChunkElectrons, 
-                                "Set the size of electron chunk for tasking");
-  chunkElectronCmd.SetDefaultValue("10000");
+    // Define the command directory
+    fMessenger = new G4GenericMessenger(
+        this, "/g4hpc/trackingAction/", "User Tracking Action control");
 
-  auto& chunkPhotonCmd
-    = fMessenger->DeclareMethod("setChunkPhotons",
-                                &TrackingAction::SetChunkPhotons, 
-                                "Set the size of photon chunk for tasking");
-  chunkPhotonCmd.SetDefaultValue("10000");
+    // commands for the size of paticle chunk for tasking
+    auto& chunkElectronCmd = fMessenger->DeclareMethod(
+        "setChunkElectrons",
+        &TrackingAction::SetChunkElectrons,
+        "Set the size of electron chunk for tasking");
+    chunkElectronCmd.SetDefaultValue("10000");
+
+    auto& chunkPhotonCmd = fMessenger->DeclareMethod(
+        "setChunkPhotons",
+        &TrackingAction::SetChunkPhotons,
+        "Set the size of photon chunk for tasking");
+    chunkPhotonCmd.SetDefaultValue("10000");
 }
